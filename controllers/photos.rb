c2929faa716e7a653ef75dd4a63e7c6c611f0e42
JARED ARKIN <ARKINJB@GMAIL.COM>
2015-07-25 21:52:56 -0400

    edit/update routes created. added links to index page and show page

--- controllers/photos.rb
+++ controllers/photos.rb
@@ -1,11 +1,13 @@
 #index routes
 get "/" do
   @photos = Photo.all
+  @random_photo = Photo.find(1 + Random.rand(@photos.length))
   erb :"photos/index"
 end
 
 get "/photos" do
   @photos = Photo.all
+  @random_photo = Photo.find(1 + Random.rand(@photos.length))
   erb :"photos/index"
 end
 
@@ -16,12 +18,29 @@ end
 
 #create route
 post "/photos" do
-  @new_photo = Photo.create(params[:photo])
-  redirect "/photos/#{@new_photo.id}"
+  @new_photo = Photo.new(params[:photo])
+  if @new_photo.save
+    redirect "/photos/#{@new_photo.id}"
+  else
+    erb :"photos/new"
+  end
 end
 
 #show route
 get "/photos/:id" do
   @photo = Photo.find(params[:id])
   erb :"photos/show"
+end
+
+#edit route
+get "/photos/:id/edit" do
+  @photo = Photo.find(params[:id])
+  erb :"photos/edit"
+end
+
+#update route
+put "/photos/:id" do
+  @photo = Photo.find(params[:id])
+  @photo.update(params[:photo])
+  redirect "/photos/#{@photo.id}"
 end
