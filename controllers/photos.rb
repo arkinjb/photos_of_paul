8834480784081647c7eba6179970ec777230a060
JARED ARKIN <ARKINJB@GMAIL.COM>
2015-07-25 21:20:00 -0400

    added new view and form. create route works

--- controllers/photos.rb
+++ controllers/photos.rb
@@ -1,11 +1,27 @@
-#index route
+#index routes
 get "/" do
   @photos = Photo.all
   erb :"photos/index"
 end
 
+get "/photos" do
+  @photos = Photo.all
+  erb :"photos/index"
+end
+
 #new route
+get "/photos/new" do
+  erb :"photos/new"
+end
 
 #create route
+post "/photos" do
+  @new_photo = Photo.create(params[:photo])
+  redirect "/photos/#{@new_photo.id}"
+end
 
 #show route
+get "/photos/:id" do
+  @photo = Photo.find(params[:id])
+  erb :"photos/show"
+end
